dc19082a68ef5e7718f6f40fc3dc201a21150ee3
Yourfirstname Yourlastname <your-email@example.com>
2024-08-23 10:15:03 +1000

    Added solution

--- node-lesson01/sum.js
+++ node-lesson01/sum.js
@@ -1,14 +1,15 @@
-
-
+//Task1
 /* ‘Simple mathematics’ program in node.js */
 console.log(`Assignment1: Sum of two numbers in Node.js`);
 
-/* To go with this solution, I read many forums and websites, they have similar answers and I tried to mix with what I thought would work, but I have to be honest, I couldn't make it without the help of forums, because I wasn't sure if the task*/
-
-
+//Task2
+//Fetch the arguments
 //Store them in variables
 //Add the variables
 //Print the result exam: $node sum.js 20 10
+
+//Solution
+/* To go with this solution, I tried several attempts, using functions, but nothing works, I ended reading many forums and websites and I found they have similar answers. I tried to mix it with what I thought would work, but I have to be honest, I couldn't make it without the help of forums */
 const myArgv = process.argv.slice(2);
 let sum = 0;
 
@@ -16,3 +17,5 @@ myArgv.forEach((val) => {
   sum+= Number(val); 
 
 });
+
+console.log(sum);
