3e19f0e894129b85546e354477596ab57f5e74d8
Yourfirstname Yourlastname <your-email@example.com>
2024-08-28 17:35:52 +1000

    Added activity 2

---
+++ alicia_ray/students.js
@@ -0,0 +1,22 @@
+//3- Use module.exports to export an array of students.
+//array of objects
+
+module.exports = 
+[
+  {
+    "firstName" : "Alicia",
+    "lastName" : "Ray",
+  },
+  {
+     "firstName" : "John",
+    "lastName" : "Doe",
+  },
+   {
+     "firstName" : "Adam",
+    "lastName" : "Smith",
+  },
+   {
+     "firstName" : "Fran",
+    "lastName" : "Murphy"
+  }
+];
