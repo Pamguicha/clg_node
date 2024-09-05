391f8b2469d49f7bafb073fcabf998d001766ca5
Yourfirstname Yourlastname <your-email@example.com>
2024-09-05 14:15:13 +1000

    Added function file and unit testing for object with functions: sum, rest, multiplication.

---
+++ node-lesson03/calculate.spec.js
@@ -0,0 +1,18 @@
+
+const mathOperations = require('./calculate');
+
+
+describe("Unit test validating addition of 2 numbers", () => {
+  it('Should throw an addition', () => {
+    expect(mathOperations.sum(8,5)).toBe(13);
+  });
+  it('Should return false', () => {
+    expect(mathOperations.sum(2,'5') == 'number').toBe(true);
+  });
+
+  it('Should return a subtraction', () => {
+    expect(mathOperations.subtract(4,2)).toBe(2);
+  });
+
+
+});
