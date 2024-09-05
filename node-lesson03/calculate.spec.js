4b896e1f3af09a08d5a7652f47186c7e022057c5
Yourfirstname Yourlastname <your-email@example.com>
2024-09-05 15:01:48 +1000

    Added multiplication test

--- node-lesson03/calculate.spec.js
+++ node-lesson03/calculate.spec.js
@@ -7,13 +7,13 @@ describe("Unit test validating addition of 2 numbers", () => {
     expect(mathOperations.sum(8,5)).toBe(13);
   });
   it('Should return false', () => {
-    expect(mathOperations.sum(2,'5') == 'number').toBe(true);
+    expect(mathOperations.sum(2,'5') == 'number').toBe(false);
   });
 
   it('Should return a subtraction', () => {
     expect(mathOperations.subtract(4,2)).toBe(2);
   });
-
+  
 if('Should return a multiplication', () => {
   expect(mathOperations.multiply(12,4)).toBe(48); 
 });
