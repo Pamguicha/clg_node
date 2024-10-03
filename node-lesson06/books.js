21cc08d970550236006093b00df6ed7a0c66df98
Yourfirstname Yourlastname <your-email@example.com>
2024-10-03 15:29:08 +1000

    Added code with bug

---
+++ node-lesson06/books.js
@@ -0,0 +1,21 @@
+
+const books = [
+  {
+    "Title" : "Lolita",
+    "Author" : "Vladimir Nabokov",
+  },
+  {
+     "Title" : "Harry Potter",
+    "Author" : "J. K. Rowling",
+  },
+   {
+     "Title" : "Weyward",
+    "Author" : "Emilia Hart",
+  },
+   {
+     "Title" : "Broken Girl",
+    "Author" : "Bradley Trevor Greive and Caroline Laner Breure",
+  }
+];
+
+ module.exports = books;
--- node-lesson06/index.js
+++ node-lesson06/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const books = require('./books');
+
 
 const app = express();
 app.use(bodyparser.json());
@@ -8,14 +10,40 @@ app.get('/', (req, res) => {
   res.json(req.body);
 })
 
-app.get('/expense', (req,res) => {
-  res.json({"Name":"Pamela", "expense":"clothes", "amount": 200})
+app.get('/books', (req,res) => {
+  res.json(books)
 
 })
 
-app.post('api/users', (req, res) => {
-  let user
+//add a new book
+app.post('/api/newbook', (req, res) => {
+  let title = req.body.title;
+  let author = req.body.author;
+  return res.send(`A new book is been added:  ${title} by ${author}.`)
 })
+
+//update a book
+app.put('/api/books/:title', (req, res) => {
+  const title  = req.params.title; // Get the book title from the URL
+  const author  = req.body.author; // Get author from the request body
+  
+  const book = books.find(bookFound => bookFound.Title === title );
+  
+  if (book) {
+    book.Author = author; // Modify the author
+    book.Title = title;
+    return res.send(`Book updated: ${title} by ${author}`);
+  } else {
+    return res.status(404).send(`Book not found: ${title}`);
+  }
+});
+
 
 
 app.listen(3000, () => console.log('server started'));
+
+
+/* TEST FOR API
+pm.test("Status code is 200", function() {
+    pm.response.to.have.status(200);
+})*/
